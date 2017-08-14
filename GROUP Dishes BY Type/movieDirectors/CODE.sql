11cfa795ded8cb2ab722b88e0054de16dbb71ae3
Marco <domandominguez@hotmail.com>
2017-08-14 11:40:37 -0700

    Update CODE.sql

--- GROUP Dishes BY Type/movieDirectors/CODE.sql
+++ GROUP Dishes BY Type/movieDirectors/CODE.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE movieDirectors()
 BEGIN
 	SELECT director FROM moviesInfo
-    WHERE oscars > 2 and year > 2000
-    GROUP BY director
-    ORDER BY director ASC;
+    	WHERE year > 2000
+    	GROUP BY director HAVING SUM(oscars) > 2
+    	ORDER BY director;
 END
