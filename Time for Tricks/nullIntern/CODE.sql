71be322b30720084e57d2fdbb81a5aab1f50699c
Marco <domandominguez@hotmail.com>
2017-08-14 16:36:05 -0700

    Update CODE.sql

--- Time for Tricks/nullIntern/CODE.sql
+++ Time for Tricks/nullIntern/CODE.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE nullIntern()
 BEGIN
 	SELECT COUNT(id) number_of_nulls FROM departments
-    WHERE  description IS NULL
-        OR UPPER(TRIM(description)) = 'NULL'
-        OR UPPER(TRIM(description)) = 'NIL'
-        OR TRIM(description) = '-'; 
+	WHERE  description IS NULL
+	OR UPPER(TRIM(description)) = 'NULL'
+	OR UPPER(TRIM(description)) = 'NIL'
+	OR TRIM(description) = '-'; 
 END
