ae7f89420dcb1027ae2e9cedab58413c74f01389
Marco <domandominguez@hotmail.com>
2017-08-23 18:58:40 -0700

    Create CODE.sql

---
+++ Table Metamorphoses/tableSecurity/CODE.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE tableSecurity()
+BEGIN
+    CREATE OR REPLACE VIEW emp
+    AS SELECT id, name, YEAR(date_joined) AS date_joined, REPLACE(salary, salary, '-') AS salary
+    FROM employees;
+
+    SELECT id, name, date_joined, salary
+    FROM emp
+    ORDER BY id;
+END
