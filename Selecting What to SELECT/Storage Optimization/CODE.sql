ff01f5509ae5c69da7d618a3ac7292dbc056d606
Marco <domandominguez@hotmail.com>
2018-03-12 16:04:47 -0700

    Update CODE.sql

--- Selecting What to SELECT/Storage Optimization/CODE.sql
+++ Selecting What to SELECT/Storage Optimization/CODE.sql
@@ -1,1 +1,17 @@
-
+CREATE PROCEDURE storageOptimization()
+BEGIN
+SELECT e.id, e.column_name, e.value FROM(
+SELECT i.id as id, 'name' as column_name, i.name as value
+from workers_info i
+where i.name != '' 
+UNION ALL
+SELECT w.id as id, 'date_of_birth' as column_name,w.date_of_birth as value
+from workers_info w
+where w.date_of_birth != '' 
+UNION ALL
+SELECT z.id as id, 'salary' as column_name, z.salary as value
+from workers_info z
+where z.salary != ''
+) e
+order by e.id, FIELD(e.column_name, 'name', 'date_of_birth', 'salary');
+END
