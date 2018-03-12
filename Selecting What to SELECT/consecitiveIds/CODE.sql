80e153240c17762f3f7fc63a6642655a1ed274a2
Marco <domandominguez@hotmail.com>
2018-03-12 16:14:08 -0700

    Update CODE.sql

--- Selecting What to SELECT/consecitiveIds/CODE.sql
+++ Selecting What to SELECT/consecitiveIds/CODE.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE consecutiveIds()
 BEGIN
-	  SELECT x.id AS oldId, COUNT(1) AS newId
+    SELECT x.id AS oldId, COUNT(1) AS newId
     FROM itemIds x
     INNER JOIN itemIds y ON y.id <= x.id
     GROUP BY x.id
