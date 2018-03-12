efbb8e3e81ea4b40b76ea8432e879cc8c393e0af
Marco <domandominguez@hotmail.com>
2018-03-12 14:56:45 -0700

    Update CODE.sql

--- Always Leave the Table in Order/mostExpensive/CODE.sql
+++ Always Leave the Table in Order/mostExpensive/CODE.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE mostExpensive()
 BEGIN
-	  SELECT name FROM Products
-    WHERE (price * quantity) = (SELECT MAX(price * quantity) FROM Products)
-    ORDER BY name
-    LIMIT 1;
+	SELECT name FROM Products
+    	WHERE (price * quantity) = (SELECT MAX(price * quantity) FROM Products)
+	ORDER BY name
+    	LIMIT 1;
 END
