94b82e4729fa07b3447c953f5993478220a165b6
Marco <domandominguez@hotmail.com>
2018-03-12 18:04:00 -0700

    Update CODE.sql

--- Exotic Dishes/findTable/CODE.sql
+++ Exotic Dishes/findTable/CODE.sql
@@ -1,14 +1,14 @@
 CREATE PROCEDURE findTable()
 BEGIN
     SELECT 
-        c.TABLE_NAME AS tab_name, 
-        c.COLUMN_NAME AS col_name,
-        c.DATA_TYPE AS data_type
-    FROM INFORMATION_SCHEMA.COLUMNS c
+        a.TABLE_NAME AS tab_name, 
+        a.COLUMN_NAME AS col_name,
+        a.DATA_TYPE AS data_type
+    FROM INFORMATION_SCHEMA.COLUMNS a
         JOIN(
             SELECT table_name FROM information_schema.tables 
-            where table_name LIKE BINARY 'e%s') tt
-    ON tt.table_name=c.table_name
-    WHERE c.TABLE_SCHEMA = 'ri_db'
-    ORDER BY c.TABLE_NAME, c.COLUMN_NAME;
+            where table_name LIKE BINARY 'e%s') b
+    ON b.table_name=a.table_name
+    WHERE a.TABLE_SCHEMA = 'ri_db'
+    ORDER BY a.TABLE_NAME, a.COLUMN_NAME;
 END
