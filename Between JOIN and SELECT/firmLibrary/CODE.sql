0df4fa1e7f54b7fb6c572ea5fdec888de88d3563
Marco <domandominguez@hotmail.com>
2018-03-13 15:49:57 -0700

    Update CODE.sql

--- Between JOIN and SELECT/firmLibrary/CODE.sql
+++ Between JOIN and SELECT/firmLibrary/CODE.sql
@@ -1,13 +1,13 @@
 CREATE PROCEDURE filmLibrary()
 BEGIN
 	SELECT a.actor as actor, a.age as age 
-  FROM actor_ages a JOIN starring_actors s ON  s.actor = a.actor
-  JOIN movies m ON s.movie_name =  m.movie
-	WHERE (m.genre) = (
-        SELECT genre FROM movies
-        GROUP BY genre
-        ORDER BY count(movie) DESC
-        LIMIT 1
-  )
-  ORDER BY a.age DESC, a.actor;
+  	FROM actor_ages a JOIN starring_actors s ON  s.actor = a.actor
+  	JOIN movies m ON s.movie_name =  m.movie
+		WHERE (m.genre) = (
+        	SELECT genre FROM movies
+        	GROUP BY genre
+        	ORDER BY count(movie) DESC
+        	LIMIT 1
+  	)
+  	ORDER BY a.age DESC, a.actor;
 END
