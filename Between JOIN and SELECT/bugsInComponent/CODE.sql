c34e9a5726f8c70385b4642ec29a502d3facc324
Marco <domandominguez@hotmail.com>
2018-03-13 15:54:22 -0700

    Update CODE.sql

--- Between JOIN and SELECT/bugsInComponent/CODE.sql
+++ Between JOIN and SELECT/bugsInComponent/CODE.sql
@@ -1,1 +1,10 @@
-
+CREATE PROCEDURE bugsInComponent()
+BEGIN
+    SELECT bt.title bug_title, ct.title component_title, 
+        (SELECT COUNT(*) FROM BugComponent 
+         WHERE component_id = id) bugs_in_component 
+    FROM Bug bt, Component ct, BugComponent
+    WHERE id = component_id && bug_num = num &&
+        (SELECT COUNT(*) FROM BugComponent WHERE bug_num = num) > 1
+    ORDER BY bugs_in_component DESC, id, num;
+END
