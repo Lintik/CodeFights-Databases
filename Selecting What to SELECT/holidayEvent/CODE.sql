5ad02985055a6af1314527244bf5850b147f0233
Marco <domandominguez@hotmail.com>
2018-03-12 17:40:57 -0700

    Update CODE.sql

--- Selecting What to SELECT/holidayEvent/CODE.sql
+++ Selecting What to SELECT/holidayEvent/CODE.sql
@@ -1,1 +1,10 @@
-
+CREATE PROCEDURE holidayEvent()
+BEGIN
+    SELECT DISTINCT buyer_name winners 
+    FROM(
+        SELECT buyer_name, @i:=@i+1 num
+        FROM purchases, (SELECT @i:=0) t
+    ) w
+    WHERE num % 4 = 0
+    ORDER BY 1;
+END
