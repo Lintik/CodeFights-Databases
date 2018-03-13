771f89b071638c3325860e80ac11331b31c7e85a
Marco <domandominguez@hotmail.com>
2018-03-12 17:47:52 -0700

    Update CODE.sql

--- Express your CREATivity/orderAnalytics/CODE.sql
+++ Express your CREATivity/orderAnalytics/CODE.sql
@@ -1,1 +1,16 @@
+DROP PROCEDURE IF EXISTS orderAnalytics;
+CREATE PROCEDURE orderAnalytics()
+BEGIN
+    
+    CREATE OR REPLACE VIEW order_analytics AS
+    SELECT id,
+        year(order_date) as year,
+        quarter(order_date) as quarter,
+        type,
+        quantity * price as total_price
+    FROM orders;
 
+    SELECT *
+    FROM order_analytics
+    ORDER by id;
+END;
