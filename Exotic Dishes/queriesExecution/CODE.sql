dfefb24cfc0556c868ea4a03cc6d4a0a14423b1b
Marco <domandominguez@hotmail.com>
2018-03-12 18:40:59 -0700

    Update CODE.sql

--- Exotic Dishes/queriesExecution/CODE.sql
+++ Exotic Dishes/queriesExecution/CODE.sql
@@ -1,1 +1,15 @@
+CREATE PROCEDURE queriesExecution()
+BEGIN
+    SET @a = CONCAT(
+        (SELECT group_concat(
+            concat('select "', 
+                   query_name, 
+                   '" query_name, (', 
+                   code,
+                   ') val')
+            separator ' union ') from queries),
+        ' order by 1');
 
+    prepare qry from @a;
+    execute qry;
+END
