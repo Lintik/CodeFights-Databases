c0e220271801ff1c2402b316aca9fc65ee5d37c1
Marco <domandominguez@hotmail.com>
2018-03-12 17:43:43 -0700

    Update CODE.sql

--- Selecting What to SELECT/hostnamesOrdering/CODE.sql
+++ Selecting What to SELECT/hostnamesOrdering/CODE.sql
@@ -1,1 +1,9 @@
-
+CREATE PROCEDURE hostnamesOrdering()
+BEGIN
+    SELECT id, hostname FROM hostnames
+    GROUP BY CONCAT(
+        SUBSTRING_INDEX(
+            CONCAT('...',hostname),'.',-1),
+            ' ', SUBSTRING_INDEX(CONCAT('...',hostname),'.',-2),
+            ' ', hostname);
+END
