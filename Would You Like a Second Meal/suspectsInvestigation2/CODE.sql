6de2b043e5839c11a9d339e07de7b03651c183f8
Marco <domandominguez@hotmail.com>
2018-07-26 11:41:01 -0700

    Update CODE.sql

--- Would You Like a Second Meal/suspectsInvestigation2/CODE.sql
+++ Would You Like a Second Meal/suspectsInvestigation2/CODE.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE suspectsInvestigation2()
 BEGIN
 	SELECT id, name, surname
-    FROM Suspect 
-    WHERE LEFT(name, 1) != 'b' OR surname NOT LIKE 'gre_n' OR height <= 170;
+    	FROM Suspect 
+    	WHERE LEFT(name, 1) != 'b' OR surname NOT LIKE 'gre_n' OR height <= 170;
 END  
